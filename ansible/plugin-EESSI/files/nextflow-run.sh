93ab2d6bb451eb2f81b7684fb7ffc930de019a3e
mirelaminkova <mirela.minkova@surf.nl>
2025-02-25 13:50:23 +0100

    this should start the modules

--- ansible/plugin-EESSI/files/nextflow-run.sh
+++ ansible/plugin-EESSI/files/nextflow-run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-source /cvmfs/software.eessi.io/versions/2023.06/init/lmod/bash
+set -e
+
+# Load EESSI environment
+if [ -f "/cvmfs/software.eessi.io/versions/2023.06/init/bash" ]; then
+    source /cvmfs/software.eessi.io/versions/2023.06/init/bash
+else
+    echo "EESSI initialization script not found! Exiting."
+    exit 1
+fi
 
 # Initialize Lmod
 if [ -f "/cvmfs/software.eessi.io/versions/2023.06/compat/linux/x86_64-001/usr/share/lmod/lmod/init/bash" ]; then
@@ -10,6 +18,9 @@ else
     export PATH="/cvmfs/software.eessi.io/versions/2023.06/compat/linux/x86_64-002/usr/share/Lmod:$PATH"
 fi
 
-module load Nextflow/23.10.0 || { echo "Failed to load Nextflow/23.10.0"; exit 1; }
-module list
-echo "PATH after loading modules: $PATH"
+# Load necessary EESSI modules for Nextflow
+#module load GCCcore/13.2.0
+module load Nextflow/23.10.0
+
+echo "EESSI environment and Nextflow module loaded."
+
